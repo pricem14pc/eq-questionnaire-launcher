5547d70496d3ab983d877a40943701d204149f45
James Gardiner <james@jgardiner.co.uk>
2020-02-04 11:44:26 +0000

    Minor changes to task and deploy

--- k8s/deploy_app.sh
+++ k8s/deploy_app.sh
@@ -7,9 +7,6 @@ if [[ -z "$RUNNER_URL" ]]; then
   exit 1
 fi
 
-DOCKER_REGISTRY="${DOCKER_REGISTRY:-eu.gcr.io/census-eq-ci}"
-IMAGE_TAG="${IMAGE_TAG:-latest}"
-
 helm tiller run \
     helm upgrade --install \
     survey-launcher \
