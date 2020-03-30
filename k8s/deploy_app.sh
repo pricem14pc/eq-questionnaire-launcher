a27f7b04793a3672f240ab434ee6940390c55658
Mebin Abraham <mebin95+work@gmail.com>
2020-03-30 11:03:24 +0100

    Add commands to restart deployment + wait for deployment to finish

--- k8s/deploy_app.sh
+++ k8s/deploy_app.sh
@@ -7,3 +7,6 @@ helm upgrade --install \
     --set surveyRunnerUrl=https://${RUNNER_URL} \
     --set image.repository=${DOCKER_REGISTRY}/eq-questionnaire-launcher \
     --set image.tag=${IMAGE_TAG}
+
+kubectl rollout restart deployment.v1.apps/launcher
+kubectl rollout status deployment.v1.apps/launcher
