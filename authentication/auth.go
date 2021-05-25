4f5e89167c1ce4d47af873f2d673d3c93e437851
petechd <53475968+petechd@users.noreply.github.com>
2021-05-25 08:51:13 +0100

    Remove census specific code (#27)

--- authentication/auth.go
+++ authentication/auth.go
@@ -143,8 +143,7 @@ func generateClaims(claimValues map[string][]string, launcherSchema surveys.Laun
 			claims[key] = value
 		}
 	}
-    var isCensusTestSchema = len(claimValues["schema_name"]) > 0 && claimValues["schema_name"][0] == "test_individual_response"
-	if !isCensusTestSchema && (len(claimValues["survey"]) > 0 || len(claimValues["form_type"]) > 0 || len(claimValues["region_code"]) > 0) {
+	if len(claimValues["form_type"]) > 0 && len(claimValues["eq_id"]) > 0 {
 		log.Println("Deleting schema name from claims")
 		delete(claims, "schema_name")
 	} else {
@@ -389,25 +388,15 @@ func GenerateTokenFromDefaults(surveyURL string, accountServiceURL string, accou
 	return token, ""
 }
 
-// TransformSchemaParamsToName Returns a schema name from census schema parameters
-// This function can be removed after census claims are removed.
+// TransformSchemaParamsToName Returns a schema name from business schema parameters
 func TransformSchemaParamsToName(postValues url.Values) string {
 	if postValues.Get("schema_name") != "" {
 		return postValues["schema_name"][0]
 	}
 
-	formTypeMap := map[string]string{
-		"H": "household",
-		"I": "individual",
-		"C": "communal_establishment",
-	}
-
-	regionCode := strings.Replace(postValues.Get("region_code"), "-", "_", -1)
-	regionCode = strings.ToLower(regionCode)
-
-	survey := postValues.Get("survey")
-	formType := formTypeMap[postValues.Get("form_type")]
-	schemaName := fmt.Sprintf("%s_%s_%s", survey, formType, regionCode)
+	eqId := postValues.Get("eq_id")
+	formType := postValues.Get("form_type")
+	schemaName := fmt.Sprintf("%s_%s", eqId, formType)
 
 	return schemaName
 }
