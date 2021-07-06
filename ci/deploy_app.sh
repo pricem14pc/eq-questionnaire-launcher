dae2e3ab9b3772ce7b31955652d8ed9679a1ca7d
petechd <piotr.chudziak@ons.gov.uk>
2021-07-06 11:45:46 +0100

    Clean up variables

--- ci/deploy_app.sh
+++ ci/deploy_app.sh
@@ -7,7 +7,7 @@ MIN_INSTANCES="${MIN_INSTANCES:=1}"
 MAX_INSTANCES="${MAX_INSTANCES:=1}"
 
 gcloud beta run deploy eq-questionnaire-launcher \
-    --project="$PROJECT_ID" --region="$REGION" --concurrency=250 --min-instances="$MIN_INSTANCES" --max-instances="$MAX_INSTANCES" \
+    --project="${PROJECT_ID}" --region="${REGION}" --concurrency=250 --min-instances="${MIN_INSTANCES}" --max-instances="${MAX_INSTANCES}" \
     --port=8000 --cpu=1 --memory=256M \
     --image="${DOCKER_REGISTRY}/eq-questionnaire-launcher:${IMAGE_TAG}" --platform=managed --allow-unauthenticated \
     --set-env-vars SURVEY_RUNNER_URL="https://${RUNNER_URL}"
