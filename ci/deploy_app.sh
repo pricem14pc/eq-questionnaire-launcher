c2f4bfdbee77bfda8dde41ccc0232bb8456a6081
petechd <piotr.chudziak@ons.gov.uk>
2021-07-08 11:39:43 +0100

    Remove url concatenation

--- ci/deploy_app.sh
+++ ci/deploy_app.sh
@@ -10,4 +10,4 @@ gcloud beta run deploy eq-questionnaire-launcher \
     --project="${PROJECT_ID}" --region="${REGION}" --concurrency=250 --min-instances="${MIN_INSTANCES}" --max-instances="${MAX_INSTANCES}" \
     --port=8000 --cpu=1 --memory=256M \
     --image="${DOCKER_REGISTRY}/eq-questionnaire-launcher:${IMAGE_TAG}" --platform=managed --allow-unauthenticated \
-    --set-env-vars SURVEY_RUNNER_URL="https://${RUNNER_URL}"
+    --set-env-vars SURVEY_RUNNER_URL="${RUNNER_URL}"
